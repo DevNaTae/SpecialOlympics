7de4f80cd6562d64361c5a0977f668ba6b0214bc
DevNaTae <e1351762081@live.uleam.edu.ec>
2024-05-23 01:07:46 -0500

    build: crear footer y agregar imágenes a carousel

--- Frontend/VistaResultados/src/app/model/sport-category.ts
+++ Frontend/VistaResultados/src/app/model/sport-category.ts
@@ -1,6 +1,8 @@
+import { Sport } from './sport';
+
 export interface SportCategory {
   id?: number;
-  sport?: number;
+  sport?: Sport;
   name?: string;
   description?: string;
   state?: boolean;
